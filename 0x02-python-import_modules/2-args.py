59bec261c306d9b5bb2c321f513398933fe2605a
Prosper67 <asemakehinde67@gmail.com>
2022-09-22 19:23:07 +0000

    Dynamic script

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from sys import argv
-    l = len(argv)
-    print("{:d} {:s}{:s}".format(l - 1, "argument" if l <= 2 else "arguments",
-                                 "." if l == 1 else ":"))
-    for i, s in enumerate(argv):
-        if i > 0:
-            print("{:d}: {:s}".format(i, s))
+    if len(argv) == 2:
+        print(len(argv) - 1, "argument:")
+        print(f"{len(argv) - 1}:", argv[1])
+    elif len(argv) == 1:
+        print("0", "arguments.")
+    else:
+        print(len(argv) - 1, "arguments:")
+        for i in range(1, len(argv)):
+            print(f"{i}:", argv[i])
